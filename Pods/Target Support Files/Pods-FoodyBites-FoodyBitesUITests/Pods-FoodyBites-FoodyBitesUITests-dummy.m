2c22a30445070e30a43d63ac3e4e4c15da365b0f
Rashmi-hub <rashmidhanotiya11@gmail.com>
2022-07-15 18:58:20 +0530

    pod add

---
+++ Pods/Target Support Files/Pods-FoodyBites-FoodyBitesUITests/Pods-FoodyBites-FoodyBitesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodyBites_FoodyBitesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FoodyBites_FoodyBitesUITests
+@end
---
+++ Pods/Target Support Files/Pods-FoodyBites/Pods-FoodyBites-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodyBites : NSObject
+@end
+@implementation PodsDummy_Pods_FoodyBites
+@end
